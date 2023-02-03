9863e6446017dc8daaa9a47e10dc9fec689c4b8a
Said Mozamil Sadat <s011071j@student.staffs.ac.uk>
2023-02-03 13:50:46 +0000

    Minor changes

--- Roche-Engine/Item.cpp
+++ Roche-Engine/Item.cpp
@@ -1,6 +1,16 @@
 #include "stdafx.h"
 #include "Item.h"
 
+Item::Item(ItemType type, int cost)
+{
+	m_itemType = type;
+	m_itemCost = cost;
+}
+
+Item::~Item()
+{
+}
+
 void Item::HandleEvent(Event* event)
 {
 }
--- Roche-Engine/Item.h
+++ Roche-Engine/Item.h
@@ -1,14 +1,23 @@
 #pragma once
 #include "EventSystem.h"
 
+enum class ItemType
+{
+	CarrotItem,
+	PotatoItem
+};
+
 class Item : Listener
 {
 	int m_itemCost;
 	std::string m_itemName;
+	ItemType m_itemType;
 
 public:
+	Item(ItemType type, int cost);
+	~Item();
+
 	// Inherited via Listener
 	virtual void HandleEvent(Event* event) override;
 	void AddToEvent() noexcept;
 };
-
